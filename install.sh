2cea905ebeb2c7aa74186e336744b875b4199dd7
Adauto Afonso de Paula <aragorn1308@gmail.com>
2022-07-03 02:58:18 -0300

    Updating install.sh script

--- install.sh
+++ install.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh;
-
-brew install vim;
-
-brew install tmux;
-
 VIMRC=~/.vimrc
+TMUXCONF=~/.tmux.conf
 
-if [ ! -f "$VIMRC" ]; then
-  echo "creating the vimrc file"
-  cp .vimrc ~/.vimrc
-else
-  echo "replacing the vimrc file content"
-  cat .vimrc > ~/.vimrc
-fi
+install_homebrew() {
+  echo "Installing homebrew"
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh;
+}
+
+install_vim() {
+  echo "Installing vim"
+  brew install vim;
+  if [ ! -f "$VIMRC" ]; then
+    echo "Creating the vimrc file"
+    cp .vimrc ~
+    echo "Copying vim configs"
+    cp -r .vim ~
+  else
+    echo "Updating the vimrc file content"
+    cat .vimrc > ~/.vimrc
+    echo "Copying vim configs"
+    rm -rf ~/.vim
+    cp -r .vim ~
+  fi
+}
+
+install_tmux() {
+  echo "Installing tmux"
+  brew install tmux;
+
+  if [ ! -f "$TMUXCONF" ]; then
+    echo "Creating the tmux conf file"
+    cp .tmux.conf ~
+  else
+    echo "Updating the tmux conf file content"
+    cat .tmux.conf > ~/.tmux.conf
+  fi
+}
+
+while true; do
+    read -p "This script will override all Vim configs. Do you want to proceed?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+install_homebrew & install_vim & install_tmux
