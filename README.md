41cd5862df73e81393a157a119ffe8efb80c2fd4
Adauto Afonso de Paula <aragorn1308@gmail.com>
2022-07-03 18:52:45 -0300

    Updating readme and installation script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# configs
+# Configs
 Tmux, Vim(version 9) and zsh configs 
+
+## Install
+```bash
+  $ curl -fsSL https://raw.githubusercontent.com/adauto/configs/main/install.sh
+```
--- install.sh
+++ install.sh
@@ -3,19 +3,37 @@
 VIMRC=~/.vimrc
 TMUXCONF=~/.tmux.conf
 
-install_homebrew() {
-  echo "Installing homebrew"
-  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+function is_bin_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
 }
 
-install_zsh() {
-  echo "Installing zsh and oh-my-zsh"
-  brew install zsh \ 
-    && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \ 
-    && cp -r .oh-my-zsh/ ~
+function install_homebrew() {
+  if [[ ! is_bin_in_path brew ]]; then
+    echo "Installing homebrew"
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  else 
+    echo "homebrew detected"
+  fi 
 }
 
-install_vim() {
+function install_zsh() {
+  if [[ ! -n $ZSH_VERSION ]]; then
+    echo "Installing zsh and oh-my-zsh"
+    brew install zsh \ 
+      && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" \ 
+      && cp -r .oh-my-zsh/ ~ \
+      #Disabling terminal auto title
+      && sed -i'.zshrc' 's/# DISABLE_AUTO_TITLE="false"/DISABLE_AUTO_TITLE="true"/g' ~/.zshrc
+  else 
+    echo "zsh detected"
+  fi
+}
+
+function install_vim() {
   echo "Installing vim"
   brew install vim
 
@@ -30,7 +48,24 @@ install_vim() {
   cp -r .vim/ ~
 }
 
-install_tmux() {
+function install_fzf() {
+  if [[ ! is_bin_in_path fzf ]]; then
+    echo "Installing fzf"
+    brew install fzf \
+      && $(brew --prefix)/opt/fzf/install
+  else 
+    echo "fzf detected"
+  fi
+  if [[ ! is_bin_in_path bat ]]; then
+      # Bat is rquired for syntax highlighting in fzf file preview
+      echo "Installing bat"
+      brew install bat
+  else 
+    echo "bat detected"
+  fi
+}
+
+function install_tmux() {
   echo "Installing tmux"
   brew install tmux;
 
@@ -52,4 +87,4 @@ while true; do
     esac
 done
 
-install_homebrew && install_zsh && install_vim && install_tmux
+install_homebrew && install_zsh && install_vim && install_fzf && install_tmux
