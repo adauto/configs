21e29d257806aaa3dede613956b9892b3b19adaa
Adauto Afonso de Paula <aragorn1308@gmail.com>
2022-07-04 20:15:44 -0300

    Automatically install vim plugins

--- install.sh
+++ install.sh
@@ -64,7 +64,7 @@ install_vim() {
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-  #vim -c 'PlugInstall'
+  vim -c 'PlugInstall' -c 'q' -c 'q'
 }
 
 install_fzf() {
